0206e6209d28936a5dbb505ee934caab52a2afd5
Hugo <hugo_math@etu.u-bourgogne.fr>
2020-03-01 08:12:45 +0100

    schedule responsive, vertical stepper

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatStepper} from '@angular/material';
 import {AuthServiceService} from '../service/auth-service.service';
+
 
 @Component({
   selector: 'app-schedule',
@@ -14,39 +15,67 @@ import {AuthServiceService} from '../service/auth-service.service';
 })
 export class ScheduleComponent implements OnInit {
   @ViewChild('stepper') stepper: MatStepper;
+
+  screenHeight: number;
+  screenWidth: number;
   isLinear: boolean;
   isEditable: boolean;
   stateGrowth: number;
   selectedIndex: number;
-  constructor(private authService: AuthServiceService) { }
+  plusDay: number;
+  innerWidth: any;
 
+
+
+  constructor(private authService: AuthServiceService) {
+    this.getScreenSize();
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenHeight, this.screenWidth);
+  }
   ngOnInit(): void {
     this.isLinear = false;
     this.isEditable = true;
     this.stateGrowth = this.authService.getStateGrowth();
-    console.log("STATE : "+this.stateGrowth);
+    console.log('STATE : ' + this.stateGrowth);
     this.selectedIndex = this.stateGrowth;
     this.isLinear = true;
     this.isEditable = false;
+    console.log('Day + ' + Math.round(this.getPlusDay()));
+    this.plusDay = Math.round(this.getPlusDay());
+    this.innerWidth = window.innerWidth;
+    console.log("Width: " + this.innerWidth);
+
   }
   move(index: number) {
     this.stepper.selectedIndex = index;
   }
 
-  selectionChange(stepper: MatStepper)
-  {
-    console.log("Selection changed");
-    console.log("Index selection changed : "+this.stepper.selectedIndex);
+  /*
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }*/
+  getPlusDay() {
+    const DatePlant = this.authService.getDatePlant();
+    const currentDate = new Date();
+    return (currentDate.getTime() - new Date(DatePlant.toString()).getTime()) / ( 1000 * 24 * 60 * 60);
+  }
+  selectionChange(stepper: MatStepper) {
+    console.log('Selection changed');
+    console.log('Index selection changed : ' + this.stepper.selectedIndex);
     this.authService.updateStateGrowth(this.stepper.selectedIndex + 1);
   }
   next(): void {
     this.stepper.linear = false;
     this.stepper.selectedIndex++;
-    console.log("Index next: "+this.stepper.selectedIndex);
+    console.log('Index next: ' + this.stepper.selectedIndex);
     this.stepper.linear = true;
   }
   reset(): void {
-    console.log("Reset !");
+    console.log('Reset !');
     this.stepper.linear = false;
     this.stepper.reset();
     this.stepper.linear = true;
